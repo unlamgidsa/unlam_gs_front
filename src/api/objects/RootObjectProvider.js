71f21d82b1b7b29607b573cc93f6f0086d9d1aed
NewtonGauss <germanmerkel9@gmail.com>
2020-10-27 12:44:37 -0300

    UNLaM UGS instead of OpenMCT

--- src/api/objects/RootObjectProvider.js
+++ src/api/objects/RootObjectProvider.js
@@ -29,7 +29,7 @@ class RootObjectProvider {
                     key: "ROOT",
                     namespace: ""
                 },
-                name: 'Open MCT',
+                name: 'UNLaM UGS',
                 type: 'root',
                 composition: []
             };
